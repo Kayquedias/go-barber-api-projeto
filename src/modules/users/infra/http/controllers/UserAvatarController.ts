6e16c00496c835ce151cb6f6e5e394618b6f7c9f
IcaroManuel <icaroomanuel@gmail.com>
2022-12-30 16:33:20 -0300

    change: Tsyring in UserAvatarController

--- src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express'
 
+import { container } from 'tsyringe'
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService'
 
 export default class UsersAvatarController {
   public async update(request: Request, response: Response): Promise<Response> {
-    const updateUserAvatar = new UpdateUserAvatarService()
+    const updateUserAvatar = container.resolve(UpdateUserAvatarService)
 
     const user = await updateUserAvatar.execute({
       user_id: request.user.id,
